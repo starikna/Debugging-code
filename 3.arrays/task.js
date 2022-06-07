7d1038ac1f8821611bb82cea5cd0f488df3b5148
starikna <don.strekoza@gmail.com>
2022-06-07 23:12:10 +0500

    first commit for home work3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,14 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  return (
+    arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx])
+  ); // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr
+    .filter((number) => number > 0)
+    .filter((number) => number % 3 === 0)
+    .map((number) => number * 10);
 
   // Ваш код
 
