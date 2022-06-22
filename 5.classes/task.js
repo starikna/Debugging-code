b426cff4367952329c23e9050b3343cbd518abdc
starikna <don.strekoza@gmail.com>
2022-06-22 23:48:08 +0500

    first commit variables classes

---
+++ 5.classes/task.js
@@ -0,0 +1,62 @@
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+  }
+
+  fix() {
+    if (this.state < 100) {
+      return (this.state = this.state * 1.5);
+    } else {
+      return (this.state = 100);
+    }
+  }
+
+  set state(number) {
+    if (number < 0) {
+      this.state = 0;
+    } else if (number > 100) {
+      this.state = 100;
+    } else {
+      this._state = number;
+    }
+  }
+
+  get state() {
+    return this._state;
+  }
+}
+
+class Magazine extends PrintEditionItem {
+  type = "magazine";
+}
+
+class Book extends PrintEditionItem {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+  }
+  type = "book";
+}
+
+class NovelBook extends Book {
+  type = "novel";
+}
+
+class FantasticBook extends Book {
+  type = "fantastic";
+}
+
+class DetectiveBook extends Book {
+  type = "detective";
+}
+
+class Library {
+  constructor(name, books) {
+    this.name = name.toString();
+    this.books = [];
+  }
+}
